4631fc03a34137bb8af8858020f6127c48644c7e
Celestine Chen <celestine@celestines-air.lan>
2021-02-05 21:23:08 +0800

    Components Part 2

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import logo from './logo.svg';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
 class App extends Component {
+  //we need to define the state in here, thats why we add constructor
+  //in order to store the state u need to define the state in the constructor of the class component
+
+  constructor(props) {
+    super(props);
+
+    this.state = {      //define state here
+      dishes: DISHES    //js object (dishes): DISHES that we've just imported
+    };
+  }
+  //after lifting this state^ into the App.js file , we can make those dishes available as props to the Menu Component
+
   render() {
     return (
       // <div className="App"> because we removed App.css (dont want them to be centered)
@@ -14,7 +27,11 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu/>   {/*This way, Menu component will be rendered below navbar*/}
+        {/*This way, Menu component will be rendered below navbar*/}
+        <Menu dishes={this.state.dishes}/>       
+        {/*Making dishes available as props
+        this way the dishes that are defined in the state for the App component is now made available as props to menu component. 
+        So, can go into my menu component, and this dishes information becomes available into menu component*/}
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Media } from 'reactstrap';
+// import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class Menu extends Component {
 
@@ -9,59 +10,51 @@ class Menu extends Component {
         //defining a state for the component
         //state stores property related to this component that we can make use of
         this.state = {
-            //here we define a property called dishes which consist of 4 js objects and each js obj has an id
-            //& each id has name, image, cat, label, etc...
-            dishes: [
-                //inside dishes we define this as a js object which contains a list of dishes
-                {
-                    id:0,
-                    name: 'Uthappizza',
-                    image: 'assets/images/uthappizza.png',
-                    category: 'mains',
-                    label: 'Hot',
-                    price: '4.99',
-                    description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-                },
-                {
-                    id: 1,
-                    name:'Zucchipakoda',
-                    image: 'assets/images/zucchipakoda.png',
-                    category: 'appetizer',
-                    label:'',
-                    price:'1.99',
-                    description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-                },
-                {
-                    id: 2,
-                    name:'Vadonut',
-                    image: 'assets/images/vadonut.png',
-                    category: 'appetizer',
-                    label:'New',
-                    price:'1.99',
-                    description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        
-                },
-                {
-                    id: 3,
-                    name:'ElaiCheese Cake',
-                    image: 'assets/images/elaicheesecake.png',
-                    category: 'dessert',
-                    label:'',
-                    price:'2.99',
-                    description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
-                }
-            ]
+            selectedDish: null
+            //null - means havent select any dish
+        }
+    }
 
+    // onDishSelect method
+    onDishSelect(dish) {
+        //now we want to update for selectedDish to point here:
+        //HOW TO CHANGE THE STATE OF YOUR COMPONENT
+        //REMEMBER, WHEN WE WANT TO CHANGE THE STATE WE NEED TO USE THIS.SETSTATE FUNCTION CALL
+        //cannot just type this.setstate=dish WRONG!!
+        this.setState({ selectedDish:dish })
+    }
+
+    //when the dish is selected^, we also want to render the details of the dish
+    renderDish(dish) {
+        if(dish != null) {
+            return(
+                <Card>
+                    <CardImg width="100%" object src={dish.image} alt={dish.name} />  
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        } else {
+            return(
+                <div></div> //empty div, nothing will render
+            )
         }
     }
 
     render() {
         //here we use the javascript map operator
-        const menu = this.state.dishes.map((dish) => {
+        // const menu = this.state.dishes.map((dish) => {
+        //previously, we have this.state={dishes[]}, but now we have dishes.js and its now props here instead of state
+        
+        const menu = this.props.dishes.map((dish) => {
+            // array.map(function(),thisvalue)
             //iterating over every dish in the dishes array here
             //for every dish we return..
             return(
-                <div key={dish.id} className="col-12 mt-5">
-                    {/* mt-5 means give a top margin of 5 units */}
+                <div key={dish.id} className="col-12 col-md-5 m-1">
+                    {/* mt-5 means give a top margin of 5 units; m-1 means margin 1 unit all around */}
                     {/* inside this div we're going to construct the view for each
                     of the items in my dishes here */}
                     {/* Whenever we construct a list of items in React, every item requires a key attribute to be specified for it 
@@ -69,22 +62,14 @@ class Menu extends Component {
                     and so the key helps when React is rendering these items on the screen.
                     The key helps React to recognise each one of these elements and while it is and while it is updating the screen so the keys will enable it to identify each of those elements uniquely. */}
                     
-                    {/* HOW TO RENDER THE LIST OF ITEMS IN THE MENU? */}
-                    <Media tag="li">
-                        {/* tag="li" says that each of these is going to act as a list item here 
-                        and we alr specified that in return() */}
-                        
-                        <Media left middle>
-                            <Media object src={dish.image} alt={dish.name} />
-                        </Media>
-
-                        <Media body className="ml-5">
-                        {/* ml-5 - give a left margin of 5 units */}
-                            <Media heading>{dish.name}</Media>
-                            <p>{dish.description}</p>
-                        </Media>
-
-                    </Media>
+                    {/* HOW TO RENDER THE LIST OF ITEMS IN THE MENU? check out git Component part 1 for Media tags*/}     
+                    <Card onClick={() => this.onDishSelect(dish)}>
+                    {/* means, on click of the card, we call this function and we pass the dish info as a parameter */}
+                        <CardImg width="100%" object src={dish.image} alt={dish.name} />  
+                        <CardImgOverlay body className="ml-5">
+                            <CardTitle>{dish.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
                 </div>
             );
         }); 
@@ -92,15 +77,20 @@ class Menu extends Component {
         return (            
         //inside here it returns what needs to be displayed on the UI by this component
             <div className="container">
+
                 <div className="row">
-                    <Media list>
                         {menu}
-                    </Media>
                 </div>
+
+                <div className="row">
+                    {this.renderDish(this.state.selectedDish)}
+                </div>
+
             </div>
         );
     }
 }
+
 
 export default Menu;
 // How to make use of this menu component? go App.js
